364c2512a88aee984fb985d2220e3e236af40b65
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-09-23 10:45:29 +0530

    added tag, category urls

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,9 @@
-from .views import PostViewSet
+from .views import PostViewSet, CategoryViewSet, TagViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = router.urls
